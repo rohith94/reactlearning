9261ac6f2ea1794a015e487ecfd86c4936a4ab00
Rohith M <rohith.m94@gmail.com>
2020-09-27 15:32:04 +0530

    added comments using faker.js made a replicable jsx code, which will be refactored for usability

--- src/index.js
+++ src/index.js
@@ -1,10 +1,58 @@
 // import
 import React from "react";
 import ReactDOM from "react-dom";
+import faker from "faker";
 
 //react component
 const App = () => {
-  return <div> Hi there .!</div>;
+  return (
+    <div className="ui container comments">
+      <div className="comment">
+        <a href="/" className="avatar">
+          <img alt="avatar" src={faker.image.avatar()} />
+        </a>
+        <div className="content">
+          <a href="/" className="author">
+            {faker.name.firstName()}
+          </a>
+          <div className="metadata">
+            <span className="date"> Today 6:00 pm</span>
+          </div>
+          <div className="text">Nice blog Post.!</div>
+        </div>
+      </div>
+
+      <div className="comment">
+        <a href="/" className="avatar">
+          <img alt="avatar" src={faker.image.avatar()} />
+        </a>
+        <div className="content">
+          <a href="/" className="author">
+            {faker.name.firstName()}
+          </a>
+          <div className="metadata">
+            <span className="date"> Today 6:00 pm</span>
+          </div>
+          <div className="text">Nice blog Post.!</div>
+        </div>
+      </div>
+
+      <div className="comment">
+        <a href="/" className="avatar">
+          <img alt="avatar" src={faker.image.avatar()} />
+        </a>
+        <div className="content">
+          <a href="/" className="author">
+            {faker.name.firstName()}
+          </a>
+          <div className="metadata">
+            <span className="date"> Today 6:00 pm</span>
+          </div>
+          <div className="text">Nice blog Post.!</div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 //render react component
